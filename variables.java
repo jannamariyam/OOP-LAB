24bc70974ff91693e280117c684ecacacbadcce7
jannamariyam <75537080+jannamariyam@users.noreply.github.com>
2021-05-05 09:07:29 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,24 @@
+class Variable {
+	public static void main(String arg[]){
+		int num; //declaration of variable
+		num=100;
+		System.out.println("this is num :"+num);
+		num=num*2;
+		System.out.print("the value of num*2 is :");
+		System.out.println(num);
+	}
+}
+
+/*   output;
+Microsoft Windows [Version 10.0.19041.928]
+(c) Microsoft Corporation. All rights reserved.
+
+C:\Users\jinu>cd C:\Users\jinu\JAVA PROGRAMS
+
+C:\Users\jinu\JAVA PROGRAMS>javac variables.java
+
+C:\Users\jinu\JAVA PROGRAMS>java Variable
+this is num :100
+the value of num*2 is :200
+
+C:\Users\jinu\JAVA PROGRAMS>
