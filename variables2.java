36e1614746bf1235c0a2bc6772a799c9ca32611e
jannamariyam <75537080+jannamariyam@users.noreply.github.com>
2021-05-05 09:07:54 +0530

    Add files via upload

---
+++ variables2.java
@@ -0,0 +1,36 @@
+class Variable {
+	public static void main(String arg[]){
+		
+		int a=5;
+		int b=2; 
+		System.out.println("a :"+a);
+  		System.out.println("b :"+b);
+		int s=a+b;
+		int sb=a-b;
+		int di=a/b;
+		int mul=a*b;
+		System.out.println("sum :"+s);
+		System.out.println("substration :"+sb);
+		System.out.println("division :"+di);
+		System.out.println("multiplication :"+mul);
+	}
+}
+
+/* output
+
+Microsoft Windows [Version 10.0.19041.928]
+(c) Microsoft Corporation. All rights reserved.
+
+C:\Users\jinu>cd C:\Users\jinu\JAVA PROGRAMS
+
+C:\Users\jinu\JAVA PROGRAMS>javac variables2.java
+
+C:\Users\jinu\JAVA PROGRAMS>java Variable
+a :5
+b :2
+sum :7
+substration :3
+division :2
+multiplication :10
+
+C:\Users\jinu\JAVA PROGRAMS>
