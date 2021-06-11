1949c3c182cf5ba70aebe4de319c1c831220d0d6
jannamariyam <75537080+jannamariyam@users.noreply.github.com>
2021-06-11 13:25:03 +0530

    Add files via upload

--- Ex3Prblm3.java
+++ Ex3Prblm3.java
@@ -2,7 +2,6 @@
  Qn.Write a menu driven program that would choose either in-built method or call a user defined method to sort an
  array of strings.
 ==================================================================================================================================================*/
-
 import java.util.Arrays;
 import java.util.Scanner;
 class Sort{
@@ -15,16 +14,16 @@ class Sort{
 		n=sc.nextInt();
 
 		//creates an array in memory of length n+1
-		String[] array = new String[n+1];	
+		String[] array = new String[n];	
 		System.out.println("Enter the strings of the array :");
-		for(i=0;i<n+1;i++){      
+		for(i=0;i<n;i++){      
 			//reading array elements from user	
-			array[i]=sc.nextLine();
+			array[i]=sc.next();
 			}
 		System.out.println("\nYour entered strings are");
 		System.out.println("************************");
 		//accessing array elements using for loop
-		for( i=0;i<n+1;i++){
+		for( i=0;i<n;i++){
 			System.out.println(array[i]);
 			}
 		System.out.println("\nsorting methods");
@@ -38,7 +37,7 @@ class Sort{
 				Arrays.sort(array) ;//in-built method
 			}
 			else if(ch==2){
-				for(i=0;i<n+1;i++)
+				for(i=0;i<n;i++)
 				{
 					for( j=i+1;j<array.length;j++)
 					{
@@ -66,10 +65,10 @@ class Sort{
 	}
 }
 
+
 /*=================================================================================================================================================
  OUTPUT   1
 ====================================================================================================================================================
-
 
 C:\Users\jinu>cd C:\Users\jinu\JAVA PROGRAMS\Experiment 3
 
@@ -85,7 +84,6 @@ DAD
 
 Your entered strings are
 ************************
-
 I
 LOVE
 MY
@@ -97,7 +95,7 @@ sorting methods
 2.User defined Method
 Enter Your choice :
 1
-Your sorted Array is : DAD I LOVE MY
+Your sorted Array is :DAD I LOVE MY
 
 =================================================================================================================================================
  OUTPUT   2
@@ -112,7 +110,6 @@ DAD
 
 Your entered strings are
 ************************
-
 I
 LOVE
 MY
@@ -124,7 +121,7 @@ sorting methods
 2.User defined Method
 Enter Your choice :
 2
-Your sorted Array is : DAD I LOVE MY
+Your sorted Array is :DAD I LOVE MY
 
 =================================================================================================================================================
  OUTPUT   3
@@ -139,7 +136,6 @@ DAD
 
 Your entered strings are
 ************************
-
 I
 LOVE
 MY
@@ -154,4 +150,5 @@ Enter Your choice :
 Invalid input
  You can't sort strings with out selecting a sorting method
 
-===================================================================================================================================*/
+==========================================================================================================================================*/
+
