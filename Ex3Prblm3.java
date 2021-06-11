6f016e87138d402ca8745d1ef356ffd5afb25198
jannamariyam <75537080+jannamariyam@users.noreply.github.com>
2021-06-11 09:55:10 +0530

    Add files via upload

--- Ex3Prblm3.java
+++ Ex3Prblm3.java
@@ -27,7 +27,7 @@ class Sort{
 		for( i=0;i<n+1;i++){
 			System.out.println(array[i]);
 			}
-		System.out.println("sorting methods");
+		System.out.println("\nsorting methods");
 		System.out.println("**************************");
 		System.out.println("1.in-built Method");
 		System.out.println("2.User defined Method");
@@ -54,15 +54,22 @@ class Sort{
 			else{
 				System.out.print("Invalid input");
 			}
+		if(ch==1 || ch==2){	
 		System.out.print("Your sorted Array is :");
 				for( i=0;i<array.length;i++)
 				System.out.print(array[i]+" ");
+			}
+		else{
+			System.out.println("\n You can't sort strings with out selecting a sorting method");
+		}
+			
 	}
 }
 
-/*=====================================================================================================================
- OUTPUT 1
-=======================================================================================================================				
+/*=================================================================================================================================================
+ OUTPUT   1
+====================================================================================================================================================
+
 
 C:\Users\jinu>cd C:\Users\jinu\JAVA PROGRAMS\Experiment 3
 
@@ -72,53 +79,79 @@ C:\Users\jinu\JAVA PROGRAMS\Experiment 3>java Sort
 Enter number of strings you want to store :4
 Enter the strings of the array :
 I
-LIKE
-RED
-CAR
+LOVE
+MY
+DAD
 
 Your entered strings are
 ************************
 
 I
-LIKE
-RED
-CAR
-sorting methods
-**************************
-1.in-built Method
-2.User defined Method
-Enter Your choice :
-2
-Your sorted Array is : CAR I LIKE RED
+LOVE
+MY
+DAD
 
-=====================================================================================================================
- OUTPUT  2
-=======================================================================================================================	
-C:\Users\jinu\JAVA PROGRAMS\Experiment 3>java Sort
-Enter number of strings you want to store :4
-Enter the strings of the array :
-I
-LIKE
-RED
-CAR
-
-Your entered strings are
-************************
-
-I
-LIKE
-RED
-CAR
 sorting methods
 **************************
 1.in-built Method
 2.User defined Method
 Enter Your choice :
 1
-Your sorted Array is : CAR I LIKE RED
+Your sorted Array is : DAD I LOVE MY
 
-=======================================================================================================================*/
-		
+=================================================================================================================================================
+ OUTPUT   2
+====================================================================================================================================================
+C:\Users\jinu\JAVA PROGRAMS\Experiment 3>java Sort
+Enter number of strings you want to store :4
+Enter the strings of the array :
+I
+LOVE
+MY
+DAD
 
+Your entered strings are
+************************
 
+I
+LOVE
+MY
+DAD
 
+sorting methods
+**************************
+1.in-built Method
+2.User defined Method
+Enter Your choice :
+2
+Your sorted Array is : DAD I LOVE MY
+
+=================================================================================================================================================
+ OUTPUT   3
+====================================================================================================================================================
+C:\Users\jinu\JAVA PROGRAMS\Experiment 3>java Sort
+Enter number of strings you want to store :4
+Enter the strings of the array :
+I
+LOVE
+MY
+DAD
+
+Your entered strings are
+************************
+
+I
+LOVE
+MY
+DAD
+
+sorting methods
+**************************
+1.in-built Method
+2.User defined Method
+Enter Your choice :
+3
+Invalid input
+ You can't sort strings with out selecting a sorting method
+
+===================================================================================================================================*/
